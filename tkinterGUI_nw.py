f25cb235b5b5b32b9ed360cc0397e9b011ce0461
YangHu <yang.hu@wsu.edu>
2023-03-04 08:59:06 -0800

    crop from original image

--- tkinterGUI_nw.py
+++ tkinterGUI_nw.py
@@ -3403,10 +3403,11 @@ def export_result(popup,segmentoutputopt,cropimageopt,hundredsize,two_hundredsiz
             minlw = min(lwthresholds)
             print('thresholds',thresholds,'lwthresholds',lwthresholds)
             imgrsc = cv2.imread(file, flags=cv2.IMREAD_ANYCOLOR)
-            imgrsc = cv2.resize(imgrsc, (originwidth,originheight),interpolation=cv2.INTER_LINEAR)
-            cropratio=findratio((originheight,originwidth),(labels.shape[0],labels.shape[1]))
-            if cropratio>1 and originheight*originwidth!=labels.shape[0]*labels.shape[1]:
-                cache = (np.zeros((originheight,originwidth)),
+            imgheight,imgwidth,imgchannel=imgrsc.shape
+            # imgrsc = cv2.resize(imgrsc, (originwidth,originheight),interpolation=cv2.INTER_LINEAR)
+            cropratio=findratio((imgheight,imgwidth),(labels.shape[0],labels.shape[1]))
+            if cropratio>1 and imgheight*imgwidth!=labels.shape[0]*labels.shape[1]:
+                cache = (np.zeros((imgheight,imgwidth)),
                      {"f": int(cropratio), "stride": int(cropratio)})
                 originconvband = tkintercorestat.pool_backward(labels, cache)
             else:
